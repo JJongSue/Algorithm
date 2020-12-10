d10dc4f03c0c5ce5bfc8f7b306251c9129bbdb95
jspark940321 <jspark940321@gmail.com>
2020-12-10 22:36:36 +0900

    solve 1039

--- src/main/java/boj/Main1254.java
+++ src/main/java/boj/Main1254.java
@@ -10,21 +10,52 @@ public class Main1254 {
         String str = br.readLine();
         int max = str.length() * 2;
         int size = str.length();
-        for(int i=size/2;i<size;i++){
-            boolean is_ok = true;
-            //자기자신비교
+        if(str.length() == 1) {
+            System.out.println(1);
+            return;
+        }
+        int start = size / 2;
+        if(size % 2 == 1) start++;
+
+        for(int i=start;i<size;i++){
             int cnt = 0;
+            boolean is_ok = true;
             for(int j=0;i+j<size;j++){
-                cnt++;
-                if(str.charAt(i+j) != str.charAt(i-j-1)) {
+                if(str.charAt(i+j) != str.charAt(i-j-1)){
                     is_ok = false;
                     break;
                 }
+                cnt++;
             }
             if(is_ok){
-                max = Math.min(max, size+size-(cnt*2));
+                max = Math.min(max, (size + size - (cnt * 2)));
+//                System.out.println("1 "+i+ " " + max);
+                break;
             }
         }
+        start = size / 2;
+//        if(size % 2 == 0) start--;
+
+        for(int i=start;i<size;i++){
+            int cnt = 0;
+            boolean is_ok = true;
+            for(int j=1;i+j<size;j++){
+                if(str.charAt(i+j) != str.charAt(i-j)){
+                    is_ok = false;
+                    break;
+                }
+                cnt++;
+            }
+            if(is_ok){
+                max = Math.min(max, (size + size - ((cnt*2)+1)));
+//                System.out.println("1 "+i+ " " + max);
+                break;
+            }
+
+        }
+        System.out.println(max);
+
+
 
     }
 }
