065a0b6fc57ebb993c60f13d5c3f71e241895d81
jspark940321 <jspark940321@gmail.com>
2020-12-27 16:15:36 +0900

    solve 2624

--- src/main/java/boj/Main2624.java
+++ src/main/java/boj/Main2624.java
@@ -16,46 +16,42 @@ public class Main2624 {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
         }
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
+//        Arrays.sort(arr, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[0] - o2[0];
+//            }
+//        });
 
-        long dp[] = new long[T+1];
+        long dp[][] = new long[K][T+1];
         int tmp = arr[0][0];
         int max = 0;
         for (int i = 0; i <= arr[0][1]; i++) {
             if(tmp * i > T) break;
             max = (tmp * i);
-            dp[tmp * i] = 1;
+            dp[0][tmp * i] = 1;
         }
-        System.out.println(Arrays.toString(dp));
+//        System.out.println(Arrays.toString(dp[0]));
 
         for (int i = 1; i < K; i++) {
-            if(arr[i][0]<T) dp[arr[i][0]]++;
-            else break;
-            for (int j = arr[i][1]; j >= 1; j--) {
-                int start = (T > max+arr[i][0] ? max+arr[i][0] : T);
-                max = Math.max(start, max);
-//                System.out.println(start);
-                for (int k = start; k > (j * arr[i][0]); k--) {
-//                    if(j>1 && k== j*arr[i][0]) continue;
-                    dp[k] += dp[k-arr[i][0]];
-
-
+            int tmpmax = 0;
+            for (int j = 0; j <= arr[i][1]; j++) {
+                for (int k = 0; k <= max; k++) {
+                    if(k+(arr[i][0]*j) > T) break;
+                    tmpmax = Math.max(tmpmax, k+(arr[i][0]*j));
+                    dp[i][k+(arr[i][0]*j)] += dp[i-1][k];
                 }
-//                System.out.println(Arrays.toString(dp));
+
             }
-            System.out.println(arr[i][0]);
-            System.out.println(Arrays.toString(dp));
+            max = Math.max(max, tmpmax);
+            max = Math.min(max, T);
+//            System.out.println(Arrays.toString(dp[i]));
         }
 
 
-        System.out.println(Arrays.toString(dp));
+//        System.out.println(Arrays.toString(dp[K-1]));
 
-        System.out.println(dp[T]);
+        System.out.println(dp[K-1][T]);
 
     }
 }
